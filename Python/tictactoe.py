0f4bdc53eb97a2296f35335b31f58f24fd7a23d7
rabitex <mmmoinronaldo@gmail.com>
2017-12-03 23:07:01 +0530

    GUI - Based on pygame. (#7)
    * Addition of file for GUI handling.
    
    * Modification of backend logic file for the same.

--- Python/tictactoe.py
+++ Python/tictactoe.py
@@ -1,188 +1,193 @@
-# -*- coding: utf-8 -*-      ##because of use of extensive UTF-8 characters as normal variable
+# -*- coding: utf-8 -*-      
 import random
+import pygame
 
-def drawBoard(board):
-     # This function prints out the board that it was passed.
-  # "board" is a list of 10 strings representing the board (ignore index 0)
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print('-----------')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('-----------')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+class Board(object):    
+    def __init__(self):
+        self.theBoard = ['-']*10
+        self.screen = pygame.display.set_mode((300, 300))
+        self.color = (100, 100, 200)
+        self.color1 = (100,200,100)
+        self.color2 = (200,100,100)
+
+    def makeMove(self,copy,letter, position):
+        if not copy:
+            self.theBoard[position] = letter
+            self.update_surface(letter,position)
+        else:
+            copy[position] = letter
+            return copy
+                
+    def update_surface(self,letter,position):
+        if letter == 'X':
+            if(position == 1):
+                pygame.draw.line(self.screen, self.color1, (30, 210), (90, 270), 10)
+                pygame.draw.line(self.screen, self.color1, (30, 270), (90, 210), 10)
+            if(position == 2):
+                pygame.draw.line(self.screen, self.color1, (120, 210), (180, 270), 10)
+                pygame.draw.line(self.screen, self.color1, (120, 270), (180, 210), 10)
+            if(position == 3):
+                pygame.draw.line(self.screen, self.color1, (210, 210), (270, 270), 10)
+                pygame.draw.line(self.screen, self.color1, (210, 270), (270, 210), 10)
+            if(position == 4):
+                pygame.draw.line(self.screen, self.color1, (30, 120), (90, 180), 10)
+                pygame.draw.line(self.screen, self.color1, (30, 180), (90, 120), 10)   
+            if(position == 5):
+                pygame.draw.line(self.screen, self.color1, (120, 120), (180, 180), 10)
+                pygame.draw.line(self.screen, self.color1, (120, 180), (180, 120), 10)
+            if(position == 6):
+                pygame.draw.line(self.screen, self.color1, (210, 120), (270, 180), 10)
+                pygame.draw.line(self.screen, self.color1, (210, 180), (270, 120), 10)
+            if(position == 7):
+                pygame.draw.line(self.screen, self.color1, (30, 30), (90, 90), 10)
+                pygame.draw.line(self.screen, self.color1, (30, 90), (90, 30), 10)
+            if(position == 8):
+                pygame.draw.line(self.screen, self.color1, (120, 30), (180, 90), 10)
+                pygame.draw.line(self.screen, self.color1, (120, 90), (180, 30), 10)
+            if(position == 9):
+                pygame.draw.line(self.screen, self.color1, (210, 30), (270, 90), 10)
+                pygame.draw.line(self.screen, self.color1, (210, 90), (270, 30), 10)
+        if letter == 'O':
+            if(position == 1):
+                pygame.draw.circle(self.screen,self.color2, (60,240), 30)
+            if(position == 2):
+                pygame.draw.circle(self.screen,self.color2, (150,240), 30)
+            if(position == 3):
+                pygame.draw.circle(self.screen,self.color2, (240,240), 30)
+            if(position == 4):
+                pygame.draw.circle(self.screen,self.color2, (60,150), 30)
+            if(position == 5):
+                pygame.draw.circle(self.screen,self.color2, (150,150), 30)
+            if(position == 6):
+                pygame.draw.circle(self.screen,self.color2, (240,150), 30)
+            if(position == 7):
+                pygame.draw.circle(self.screen,self.color2, (60,60), 30)
+            if(position == 8):
+                pygame.draw.circle(self.screen,self.color2, (150,60), 30)
+            if(position == 9):
+                pygame.draw.circle(self.screen,self.color2, (240,60), 30)    
+
+    def isWinner(self,copy,le):
+        return ((copy[7] == le and copy[8] == le and copy[9] == le) or 
+            (copy[4] == le and copy[5] == le and copy[6] == le) or 
+            (copy[1] == le and copy[2] == le and copy[3] == le) or 
+            (copy[7] == le and copy[4] == le and copy[1] == le) or 
+            (copy[8] == le and copy[5] == le and copy[2] == le) or 
+            (copy[9] == le and copy[6] == le and copy[3] == le) or 
+            (copy[7] == le and copy[5] == le and copy[3] == le) or 
+            (copy[9] == le and copy[5] == le and copy[1] == le)) 
+                
+    def getBoardCopy(self):
+        dupboard=[]
+        for i in self.theBoard:
+            dupboard.append(i)
+        return dupboard
     
-    
-
-def inputPlayerLetter():
-    print "Do you want to be X or O"
-    letter = raw_input('>')
-    letter = letter.upper()
-    if letter == 'X':
-        return ['X','O']
-    else:
-        return ['O','X']
-    
-def whoGoesFirst():
-    #randomly chooses betwwen computer and player
-    if random.randint(0,1)==0:
-        return 'computer'
-    else:
-        return 'player'
-
-
-def playAgain():
-    # func eturn true if player wants to play again
-    print "Do you want to play again (Y or N)?"
-    flag = raw_input()
-    if(flag == 'Y'):
+    def isSpaceFree(self,copy,move):
+        return copy[move] == '-'
+       
+    def chooseRandomMoveFromList(self,copy,movesList):
+        possibleMoves = []
+        for i in movesList:
+            if self.isSpaceFree(copy, i):
+                possibleMoves.append(i)
+        if len(possibleMoves) != 0:
+            return random.choice(possibleMoves)
+        else:
+            return None
+               
+    def getComputerMove(self,copy):
+        for i in range(1, 10):
+            if self.isSpaceFree(copy, i):
+                self.makeMove(copy, 'O', i)
+                if self.isWinner(copy, 'O'):
+                    return i
+        copy = self.getBoardCopy()
+        for i in range(1, 10):
+            if self.isSpaceFree(copy, i):
+                self.makeMove(copy, 'X', i)
+                if self.isWinner(copy, 'X'):
+                    return i
+        copy = self.getBoardCopy()    
+        move = self.chooseRandomMoveFromList(copy, [1, 3, 7, 9])
+        if move != None:
+            return move
+        if self.isSpaceFree(copy, 5):
+            return 5
+        return self.chooseRandomMoveFromList(copy, [2, 4, 6, 8])
+   
+    def isBoardFull(self,copy):
+        for i in range(1, 10):
+            if self.isSpaceFree(copy,i):
+                return False
         return True
-    else:
-        return False
 
+    def initialize(self):
+        
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(10, 10, 100, 10))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(10, 10, 10, 100))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(100, 10, 10, 100))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(10, 100, 100, 10))
 
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(100, 10, 90, 10))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(100, 100, 90, 10))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(190, 10, 10, 100))
 
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(200, 10, 90, 10))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(200, 100, 90, 10))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(280, 10, 10, 100))
 
-def makeMove(board, letter, move):
-    board[move] = letter
-    
-def isWinner(bo, le):
-    # Given a board and a player’s letter, this function returns True if that player has won.
-    # We use bo instead of board and le instead of letter so we don’t have to type as much.
-    return ((bo[7] == le and bo[8] == le and bo[9] == le) or # across the top
-            (bo[4] == le and bo[5] == le and bo[6] == le) or # across the middle
-            (bo[1] == le and bo[2] == le and bo[3] == le) or # across the bottom
-            (bo[7] == le and bo[4] == le and bo[1] == le) or # down the left side
-            (bo[8] == le and bo[5] == le and bo[2] == le) or # down the middle
-            (bo[9] == le and bo[6] == le and bo[3] == le) or # down the right side
-            (bo[7] == le and bo[5] == le and bo[3] == le) or # diagonal
-            (bo[9] == le and bo[5] == le and bo[1] == le)) # diagonal
-            
-            
-            
-def getBoardCopy(board):
-    #make a duplicate of the board
-    dupboard=[]
-    for i in board:
-        dupboard.append(i)
-    return dupboard
-    
-def isSpaceFree(board, move):
-    #Return true if the passed move is free on the passed board.
-    return board[move] == '-'
-    
-def getPlayerMove(board):
-    #Let the player type in their move.
-    print "What is your next move? (1-9)"
-    Flag = True
-    while Flag:
-        move = int(raw_input())
-        if move not in [1,2,3,4,5,6,7,8,9] or not isSpaceFree(board,move):
-            print 'Invalid move! \nPlease re enter move'
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(10, 100, 10, 90))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(100, 100, 10, 90))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(10, 190, 100, 10))
+
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(100, 100, 10, 90))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(190, 100, 10, 90))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(100, 190, 100, 10))
+
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(280, 100, 10, 90))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(280, 100, 10, 90))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(190, 190, 100, 10))
+
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(10, 190, 10, 90))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(100, 190, 10, 90))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(10, 280, 100, 10))
+
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(100, 190, 10, 90))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(190, 190, 10, 90))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(100, 280, 100, 10))
+
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(280, 190, 10, 90))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(280, 190, 10, 90))
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(190, 280, 100, 10)) 
+
+    def nxt_turn(self,position):
+        self.makeMove(0,'X',position)
+        if self.isWinner(self.getBoardCopy(),'X'):
+            self.display_game_over('Player')
+            return
         else:
-            Flag = False
-    return move
-    
-    
-def chooseRandomMoveFromList(board, movesList):
-    # Returns a valid move from the passed list on the passed board.
-    # Returns None if there is no valid move.
-    possibleMoves = []
-    for i in movesList:
-        if isSpaceFree(board, i):
-            possibleMoves.append(i)
-    if len(possibleMoves) != 0:
-        return random.choice(possibleMoves)
-    else:
-        return None
-        
-        
-def getComputerMove(board, playerLetter):
-    if playerLetter == 'X':
-       computerLetter = 'O'
-    else:
-       computerLetter = 'X'
-    # Here is our algorithm for our Tic Tac Toe AI:
-    # First, check if we can win in the next move
-    for i in range(1, 10):
-       copy = getBoardCopy(board)
-       if isSpaceFree(copy, i):
-           makeMove(copy, computerLetter, i)
-           if isWinner(copy, computerLetter):
-               return i
-    
-    # Check if the player could win on their next move, and block them.
-    for i in range(1, 10):
-         copy = getBoardCopy(board)
-         if isSpaceFree(copy, i):
-             makeMove(copy, playerLetter, i)
-             if isWinner(copy, playerLetter):
-                 return i
-    # Try to take one of the corners, if they are free.
-    move = chooseRandomMoveFromList(board, [1, 3, 7, 9])
-    if move != None:
-        return move
-
-    # Try to take the center, if it is free.
-    if isSpaceFree(board, 5):
-        return 5
-
-    # Move on one of the sides.
-    return chooseRandomMoveFromList(board, [2, 4, 6, 8])
-
-    
-def isBoardFull(board):
-    # Return True if every space on the board has been taken. Otherwise return False.
-    for i in range(1, 10):
-        if isSpaceFree(board, i):
-            return False
-    return True
-
-        
-print "WELCOME TO TIC TAC TOE GAME MADE BY - RKM"
-while True:
-    # Reset the board
-    theBoard = ['-'] * 10
-    playerLetter, computerLetter = inputPlayerLetter()
-    turn = whoGoesFirst()
-    print('The ' + turn + ' will go first.')
-    gameIsPlaying = True
-
-    while gameIsPlaying:
-        if turn == 'player':
-            # Player’s turn.
-            drawBoard(theBoard)
-            move = getPlayerMove(theBoard)
-            makeMove(theBoard, playerLetter, move)
-            
-
-            if isWinner(theBoard, playerLetter):
-                drawBoard(theBoard)
-                print('Hooray! You have won the game!')
-                gameIsPlaying = False
-            else:
-                if isBoardFull(theBoard):
-                   drawBoard(theBoard)
-                   print('The game is a tie!')
-                   gameIsPlaying = False
-                else:
-                   turn = 'computer'
-
+            if self.isBoardFull(self.getBoardCopy()):
+                self.display_game_over(0)
+                return
+        self.makeMove(0,'O',self.getComputerMove(self.getBoardCopy()))
+        if self.isWinner(self.getBoardCopy(),'O'):
+            self.display_game_over('Computer')
+            return
         else:
-            # Computer’s turn.
-            move = getComputerMove(theBoard, playerLetter)
-            makeMove(theBoard, computerLetter, move)
+            if self.isBoardFull(self.getBoardCopy()):
+                self.display_game_over(0)
+                return
 
-            if isWinner(theBoard, computerLetter):
-               drawBoard(theBoard) 
-               print('The computer has beaten you! You lose.')
-               gameIsPlaying = False
-            else:
-                if isBoardFull(theBoard):
-                   drawBoard(theBoard) 
-                   print('The game is a tie!')
-                   gameIsPlaying = False
-                else:
-                   turn = 'player'
+    def display_game_over(self, winner):
+        surface_size = self.screen.get_height()
+        font = pygame.font.Font('freesansbold.ttf', surface_size / 8)
+        if winner:
+            text = '%s won!' % winner
+        else:
+            text = 'Draw!'
+        text = font.render(text, True,(100,120,130), (200,220,230))
+        rect = text.get_rect()
+        rect.center = (surface_size / 2, surface_size / 2)
+        self.screen.blit(text, rect)
 
-    if not playAgain():
-       break      
-        
