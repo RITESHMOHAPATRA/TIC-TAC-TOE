2f3a71968bf8503e4b07f841a278ae9366e22357
rabitex <mmmoinronaldo@gmail.com>
2017-12-13 16:53:20 +0530

    Addition of yes and no texts in the end menu.

--- Python/tictactoe.py
+++ Python/tictactoe.py
@@ -286,3 +286,12 @@ class Board(object):
             pygame.draw.rect(self.screen, self.bright_red,(175,200,100,75))
         else:
             pygame.draw.rect(self.screen, self.red,(175,200,100,75))
+
+        text3 = font.render('Yes', True, self.black)
+        rect3 = text3.get_rect()
+        rect3.center = (75,237)
+        self.screen.blit(text3,rect3)
+        text4 = font.render('No', True, self.black)
+        rect4 = text4.get_rect()
+        rect4.center = (225,237)
+        self.screen.blit(text4,rect4)
