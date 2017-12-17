6ef17571fba1ffb6fc9daaa024841650027fc49c
Ashwin Sekhari <ashwinsekhari@gmail.com>
2017-12-17 08:23:14 +0530

    Fixed Syntax

--- Python/Mini.py
+++ Python/Mini.py
@@ -207,7 +207,7 @@ def determine(board, player):
         board.make_move(move, player)
         val = board.alphabeta(board, get_enemy(player), -2, 2)
         board.make_move(move, None)
-        print "move:", move + 1, "causes:", board.winners[val + 1]
+        print("move:", move + 1, "causes:", board.winners[val + 1])
         if val > a:
             a = val
             choices = [move]
